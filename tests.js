f85cde4f995a595f4b44848bb3292c5f042f20ff
Zeeshan Syed <zsyed89@gmail.com>
2018-11-06 00:26:54 -0500

    Added workers and tests file

---
+++ tests.js
@@ -0,0 +1,24 @@
+// var _data = require('./lib/data');
+
+// @TODO GET RID OF THIS
+// helpers.sendTwilioSms('9055801151', 'Hello', function(err) {
+//     console.log('This was the error', err);
+// });
+
+// TESTING
+// @TODO delete this
+// _data.write('test', 'newFile', { foo: 'bar' }, function(err) {
+//     console.log('This was the error', err);
+// });
+
+// _data.read('test', 'newFile', function(err, data) {
+//     console.log('This was the error', err, 'and this was the data', data);
+// });
+
+// _data.update('test', 'newFile', { fizz: 'buzz' }, function(err) {
+//     console.log('This was the error', err);
+// });
+
+// _data.delete('test', 'newFile', function(err) {
+//     console.log('This was the error', err);
+// });
