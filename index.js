14838672ea43423bdf2646ef448f48a3e1e85d97
Zeeshan Syed <zsyed89@gmail.com>
2018-11-21 21:07:38 -0500

    Scaffolding for CLI application, wrote help/man and stats commands

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
@@ -16,6 +17,11 @@ app.init = function() {
 
     // Start the workers
     workers.init();
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(function() {
+        cli.init();
+    }, 50);
 };
 
 // Execute
